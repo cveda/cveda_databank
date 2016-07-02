7975be6a61ceaedabf24679ad8295ca8ec2bb3d2
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2016-07-02 11:19:43 +0200

    Licensing

--- psc/generate_psc1.py
+++ psc/generate_psc1.py
@@ -1,11 +1,39 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# Copyright (c) 2010-2016 CEA
+#
+# This software is governed by the CeCILL license under French law and
+# abiding by the rules of distribution of free software. You can use, 
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info". 
+#
+# As a counterpart to the access to the source code and rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty and the software's author, the holder of the
+# economic rights, and the successive licensors have only limited
+# liability. 
+# 
+# In this respect, the user's attention is drawn to the risks associated
+# with loading, using, modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean that it is complicated to manipulate, and that also
+# therefore means that it is reserved for developers and experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or 
+# data to be ensured and, more generally, to use and operate it in the 
+# same conditions as regards security. 
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
 """Generate codes to pseudonymize c-VEDA data.
 
-The algorithm used here is certainly brain-dead. This is not reusable code.
-I spent 10 minutes writing and it took ~ 3 days to generate PSC1 codes for
-the c-VEDA projects. This is good enough for a one-shot run.
+The algorithm used here is certainly brain-dead. This is not reusable code
+but is good enough for a one-shot run. I spent 10 minutes writing and it
+took ~ 3 days to generate PSC1 codes for the c-VEDA projects.
 
 Notes
 -----
