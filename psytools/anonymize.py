ac7fb60d87e928e23a1d8275d53f561638066935
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2016-07-02 11:16:15 +0200

    Licensing and minor fixes

--- psytools/anonymize.py
+++ psytools/anonymize.py
@@ -1,11 +1,41 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# Copyright (c) 2010-2016 CEA
+#
+# This software is governed by the CeCILL license under French law and
+# abiding by the rules of distribution of free software. You can use, 
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info". 
+#
+# As a counterpart to the access to the source code and rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty and the software's author, the holder of the
+# economic rights, and the successive licensors have only limited
+# liability. 
+# 
+# In this respect, the user's attention is drawn to the risks associated
+# with loading, using, modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean that it is complicated to manipulate, and that also
+# therefore means that it is reserved for developers and experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or 
+# data to be ensured and, more generally, to use and operate it in the 
+# same conditions as regards security. 
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
 """Re-encode and anonymize Psytools files.
 
 This script iterates over Psytools CSV files downloaded from the Delosis
-server and saved into a directory. For each file it will:
+server. For each file it will:
 * substitute the PSC1 code for a PSC2 code,
-* change acquisition dates into age in days at acquisition.
+* change acquisition dates into age in days at acquisition,
+* remove some data (such as date of birth) used for cross-checking.
 
 ==========
 Attributes
@@ -15,18 +45,18 @@ Input
 -----
 
 PSYTOOLS_PSC1_DIR : str
-    Location of PSC1-encoded Psytools files.
+    Source directory to read PSC1-encoded Psytools files from.
 
 Output
 ------
 
 PSYTOOLS_PSC2_DIR : str
-    Location of PSC2-encoded Psytools files.
+    Target directory to write PSC2-encoded Psytools files to.
 
 """
 
-PSYTOOLS_PSC1_DIR = '/cveda/BL/RAW/PSC1/psytools'
-PSYTOOLS_PSC2_DIR = '/cveda/BL/RAW/PSC2/psytools'
+PSYTOOLS_PSC1_DIR = '/cveda/BL/raw/PSC1/psytools'
+PSYTOOLS_PSC2_DIR = '/cveda/BL/raw/PSC2/psytools'
 
 import logging
 logging.basicConfig(level=logging.INFO)
