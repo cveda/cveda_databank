fdcac58117c9d2fa3bcdb251e4d4aa207f055662
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2016-07-04 14:44:32 +0200

    Ooops... Wrong path to PSC1 and PSC2 directories

--- psytools/anonymize.py
+++ psytools/anonymize.py
@@ -55,8 +55,8 @@ PSYTOOLS_PSC2_DIR : str
 
 """
 
-PSYTOOLS_PSC1_DIR = '/cveda/BL/raw/PSC1/psytools'
-PSYTOOLS_PSC2_DIR = '/cveda/BL/raw/PSC2/psytools'
+PSYTOOLS_PSC1_DIR = '/cveda/databank/BL/raw/PSC1/psytools'
+PSYTOOLS_PSC2_DIR = '/cveda/databank/BL/raw/PSC2/psytools'
 
 import logging
 logging.basicConfig(level=logging.INFO)
