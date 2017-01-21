a4659765fdf8b3c4432ee83b116365ccc4b85104
Dimitri Papadopoulos <dimitri.papadopoulos@cea.fr>
2017-01-21 12:27:43 +0100

    Improve age / date of birth reporting

--- psytools/dob_age_debug.py
+++ psytools/dob_age_debug.py
@@ -47,12 +47,10 @@ PHIR : str
 
 """
 
-import logging
-logging.basicConfig(level=logging.INFO)
-
 import csv
 import sys
 import datetime
+import xlsxwriter
 
 ACE_IQ = '/cveda/databank/BL/RAW/PSC1/psytools/cVEDA-cVEDA_ACEIQ-BASIC_DIGEST.csv'
 PHIR = '/cveda/databank/BL/RAW/PSC1/psytools/cVEDA-cVEDA_PHIR-BASIC_DIGEST.csv'
@@ -64,12 +62,9 @@ def age(today, birth):
             - ((today.month, today.day) < (birth.month, birth.day)))
 
 
-def main():
-    cohort = {}
-
-    # ACE-IQ questionnaire
+def read_ace_iq(path):
     ace_iq = {}
-    with open(ACE_IQ, mode='r', newline='') as csvfile:
+    with open(path, mode='r', newline='') as csvfile:
         dialect = csv.Sniffer().sniff(csvfile.read(4096))
         csvfile.seek(0)
         reader = csv.DictReader(csvfile, dialect=dialect)
@@ -95,10 +90,12 @@ def main():
                             iteration_value['age'] = int(result)  # overwrite previous results
     for psc1, value in ace_iq.items():
         ace_iq[psc1] = value[max(value.keys())]  # keep last iteration only
+    return ace_iq
 
-    # PHIR questionnaire
+
+def read_phir(path):
     phir = {}
-    with open(PHIR, mode='r', newline='') as csvfile:
+    with open(path, mode='r', newline='') as csvfile:
         dialect = csv.Sniffer().sniff(csvfile.read(4096))
         csvfile.seek(0)
         reader = csv.DictReader(csvfile, dialect=dialect)
@@ -121,6 +118,47 @@ def main():
                             iteration_value['age_from_dob'] = age(completed, dob)
     for psc1, value in phir.items():
         phir[psc1] = value[max(value.keys())]  # keep last iteration only
+    return phir
+
+
+def main():
+    # ACE-IQ questionnaire
+    ace_iq = read_ace_iq(ACE_IQ)
+
+    # PHIR questionnaire
+    phir = read_phir(PHIR)
+
+    # Excel output
+    options = {
+        'strings_to_numbers': False,
+    }
+    workbook = xlsxwriter.Workbook('cVEDA_Psytools.xlsx', options)
+    HEADER_FORMAT = workbook.add_format({'bold': True,
+                                         'align': 'center', 'valign': 'vcenter'})
+    worksheet = workbook.add_worksheet('date of birth - age')
+    # PSC1
+    worksheet.merge_range(0, 0, 1, 0, u'PSC1', HEADER_FORMAT)
+    worksheet.set_column(0, 0, 14)
+    # ACE-IQ
+    worksheet.merge_range(0, 1, 0, 3, u'ACE-IQ', HEADER_FORMAT)
+    worksheet.write(1, 1, u'date of birth', HEADER_FORMAT)
+    worksheet.write(1, 2, u'age', HEADER_FORMAT)
+    worksheet.write(1, 3, u'computed age', HEADER_FORMAT)
+    worksheet.set_column(1, 3, 16)
+    # PHIR
+    worksheet.merge_range(0, 4, 0, 5, u'PHIR', HEADER_FORMAT)
+    worksheet.write(1, 4, u'date of birth', HEADER_FORMAT)
+    worksheet.write(1, 5, u'computed age', HEADER_FORMAT)
+    worksheet.set_column(4, 5, 16)
+    # more formatting and prepare for writing data
+    worksheet.freeze_panes(2, 0)
+    error_format = {'bg_color': '#FF6A6A'}
+    ERROR_FORMAT = workbook.add_format(error_format)
+    date_format = {'num_format': 'yyyy-mm-dd'}
+    DATE_FORMAT = workbook.add_format(date_format)
+    date_format.update(error_format)
+    DATE_ERROR_FORMAT = workbook.add_format(date_format)
+    row = 2
 
     for psc1 in ace_iq.keys() & phir.keys():
         ace_iq_dob = None
@@ -140,10 +178,60 @@ def main():
                 phir_dob = phir[psc1]['dob']
             if 'age_from_dob' in phir[psc1]:
                 phir_age_from_dob = phir[psc1]['age_from_dob']
-        if ace_iq_dob != phir_dob:
-            print('{} DOB: {} {}'.format(psc1, ace_iq_dob, phir_dob))
-        if ace_iq_age and phir_age_from_dob and ace_iq_age != phir_age_from_dob:
-            print('{} AGE: {} {}'.format(psc1, ace_iq_age, phir_age_from_dob))
+
+        if psc1.startswith('1'):
+            error = False
+            # date of birth
+            if ace_iq_dob and phir_dob and ace_iq_dob != phir_dob:
+                error = True
+                if ace_iq_age and (ace_iq_age == phir_age_from_dob and
+                                   ace_iq_age != ace_iq_age_from_dob):
+                    col_1 = (ace_iq_dob, DATE_ERROR_FORMAT)
+                    col_4 = (phir_dob, DATE_FORMAT)
+                elif ace_iq_age and (ace_iq_age == ace_iq_age_from_dob and
+                                   ace_iq_age != phir_age_from_dob):
+                    col_1 = (ace_iq_dob, DATE_FORMAT)
+                    col_4 = (phir_dob, DATE_ERROR_FORMAT)
+                else:
+                    col_1 = (ace_iq_dob, DATE_ERROR_FORMAT)
+                    col_4 = (phir_dob, DATE_ERROR_FORMAT)
+            else:
+                col_1 = (ace_iq_dob, DATE_FORMAT)
+                col_4 = (phir_dob, DATE_FORMAT)
+            # age
+            col_2 = (ace_iq_age, None)
+            if ace_iq_age:
+                if ace_iq_age_from_dob and ace_iq_age != ace_iq_age_from_dob:
+                    error = True
+                    age_error = True
+                    col_3 = (ace_iq_age_from_dob, ERROR_FORMAT)
+                    col_2 = (ace_iq_age, ERROR_FORMAT)
+                else:
+                    col_3 = (ace_iq_age_from_dob, None)
+                if phir_age_from_dob and ace_iq_age != phir_age_from_dob:
+                    error = True
+                    age_error = True
+                    col_5 = (phir_age_from_dob, ERROR_FORMAT)
+                    col_2 = (ace_iq_age, ERROR_FORMAT)
+                else:
+                    col_5 = (phir_age_from_dob, None)
+            else:
+                col_3 = (ace_iq_age_from_dob, None)
+                col_5 = (phir_age_from_dob, None)
+
+            if error:
+                # PSC1
+                worksheet.write_string(row, 0, psc1, ERROR_FORMAT)
+                # ACE-IQ
+                worksheet.write(row, 1, col_1[0], col_1[1])
+                worksheet.write(row, 2, col_2[0], col_2[1])
+                worksheet.write(row, 3, col_3[0], col_3[1])
+                # PHIR
+                worksheet.write(row, 4, col_4[0], col_4[1])
+                worksheet.write(row, 5, col_5[0], col_5[1])
+                row += 1
+
+    workbook.close()
 
 
 if __name__ == "__main__":
