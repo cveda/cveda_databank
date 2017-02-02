59cafb0a10439afe00e57e06bd61c61aa5f1668f
Dimitri Papadopoulos Orfanos <dimitri.papadopoulos@gmail.com>
2017-02-02 02:30:45 +0100

    Ooops...

--- mri/cveda_mri_collect.py
+++ mri/cveda_mri_collect.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os-from __future__ import print_function
+from __future__ import print_function
+import os
 import sys
 from datetime import date
 from cveda_databank.sanity import check_zip_name
