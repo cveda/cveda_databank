ff87497452b378b49f3384c6c053d77a214239c4
Dimitri Papadopoulos Orfanos <dimitri.papadopoulos@gmail.com>
2017-03-22 13:31:55 +0100

    Roll back

--- psytools/debug_dob_age.py
+++ psytools/debug_dob_age.py
@@ -61,7 +61,7 @@ logger = logging.getLogger()
 
 ACE_IQ = '/cveda/databank/RAW/PSC1/psytools/cVEDA-cVEDA_ACEIQ-BASIC_DIGEST.csv'
 PHIR = '/cveda/databank/RAW/PSC1/psytools/cVEDA-cVEDA_PHIR-BASIC_DIGEST.csv'
-EXCEL = '/cveda/databank/framework/meta_data/PSC1_DOB_2017-03-22.xlsx'
+EXCEL = '/cveda/databank/framework/meta_data/PSC1_DOB_2017-03-15.xlsx'
 
 
 def age(today, birth):
@@ -157,7 +157,7 @@ def read_excel(path):
         index = {cell.value: i for i, cell in enumerate(rows[0])
                  if cell.value}
         for row in rows[1:]:
-            psc1 = row[index['PSC1 Code']].value
+            psc1 = row[index['PSC1 CODE']].value
             if not psc1:
                 continue
             if isinstance(psc1, int):
@@ -166,7 +166,7 @@ def read_excel(path):
             if len(psc1) != 12 or not psc1.isdigit():
                 logger.warn('bogus PSC1: %s', psc1)
                 continue
-            dob = row[index['DOB']].value
+            dob = row[index['Date of Birth']].value
             if not dob:
                 continue
             if 'Comments' in index and row[index['Comments']].value:
