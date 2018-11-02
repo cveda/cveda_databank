3a3fc57c140cc457418da69cf9f1073d1eacaa11
Dimitri Papadopoulos <DimitriPapadopoulos@users.noreply.github.com>
2018-11-02 15:54:47 +0100

    Big changes for data freeze 1.0!
    * Rely solely on recruitment files for DOB/sex.
    * Add a script to extract "frozen" participants from Psytools CSV files.

--- cveda_databank/core.py
+++ cveda_databank/core.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2014-2017 CEA
+# Copyright (c) 2014-2018 CEA
 #
 # This software is governed by the CeCILL license under French law and
 # abiding by the rules of distribution of free software. You can use,
@@ -29,9 +29,8 @@
 # knowledge of the CeCILL license and that you accept its terms.
 
 import os
+import pandas
 from datetime import datetime
-from openpyxl import load_workbook
-from .psytools import read_psytools
 
 import logging
 logger = logging.getLogger(__name__)
@@ -44,10 +43,10 @@ if sys.version_info[0] < 3:
 else:
     integer_types = (int,)
 
-#
-# reference files
-#
+# PSC1 to PSC2 conversion table
 _PSC_PATH = '/cveda/databank/framework/psc/psc2psc_2018-06-23.txt'
+
+# recruitment file with reference date of birth / sex information
 _RECRUITMENT_FILES_DIR = '/cveda/databank/framework/meta_data/recruitment_files'
 _RECRUITMENT_FILES = (
     'recruitment_file_PGIMER.xlsx',
@@ -58,10 +57,6 @@ _RECRUITMENT_FILES = (
     'recruitment_file_NIMHANS.xlsx',
     'recruitment_file_SJRI.xlsx',
 )
-_ACE_IQ_PATH = '/cveda/databank/RAW/PSC1/psytools/cVEDA-cVEDA_ACEIQ-BASIC_DIGEST.csv'
-_PHIR_PATH = '/cveda/databank/RAW/PSC1/psytools/cVEDA-cVEDA_PHIR-BASIC_DIGEST.csv'
-_PDS_PATH = '/cveda/databank/RAW/PSC1/psytools/cVEDA-cVEDA_PDS-BASIC_DIGEST.csv'
-_SDIM_PATH = '/cveda/databank/RAW/PSC1/psytools/cVEDA-cVEDA_SDIM-BASIC_DIGEST.csv'
 
 
 def _initialize_psc2_from_psc1(path):
@@ -92,61 +87,26 @@ def _initialize_psc2_from_psc1(path):
     return psc2_from_psc1
 
 
-def _read_excel(path):
-    excel = {}
-    workbook = load_workbook(path)
-    for worksheet in workbook.worksheets:
-        rows = list(worksheet.rows)
-        index = {cell.value: i for i, cell in enumerate(rows[0])
-                 if cell.value}
-        if 'PSC1' not in index:
-            logging.fatal('%s: PSC1 missing from header: %s',
-                          path, index)
-        for row in rows[1:]:
-            psc1 = row[index['PSC1']].value
-            if psc1:
-                # clean up and detect invalid PSC1 codes
-                if isinstance(psc1, integer_types):
-                    psc1 = str(psc1)
-                psc1 = psc1.strip()
-                if psc1.isdigit():
-                    if len(psc1) != 12:
-                        logger.error('invalid PSC1: %s', psc1)
-                        psc1 = None
-                else:
-                    logger.warn('invalid PSC1: %s', psc1)
-                    psc1 = None
-            dob = row[index['DOB']].value
-            if dob:
-                # clean up and detect invalid dates of birth
-                if isinstance(dob, str):
-                    dob = dob.strip()
-                    try:
-                        dob = datetime.strptime(dob, '%d-%m-%Y').date()
-                    except ValueError:
-                        logger.error('invalid date of birth: %s', dob)
-                        dob = None
-                elif isinstance(dob, datetime):
-                    dob = dob.date()
-                else:
-                    logger.error('invalid date of birth: %s', str(dob))
-                    dob = None
-            if psc1 and dob:
-                excel[psc1] = dob
-    return excel
+def _read_recruitment_file(path):
+    with pandas.ExcelFile(path) as excel_file:
+        converters = {
+            'PSC1': str,
+        }
+        return pandas.read_excel(path, converters=converters)
 
 
-def _initialize_dob_from_psc1(excel_paths, ace_iq_path, phir_path):
+def _read_recruitment_files(paths):
+    return pandas.concat((_read_recruitment_file(path) for path in paths),
+                         ignore_index=True)
+
+
+def _initialize_dob_sex(paths):
     """Build dictionnary to map PSC1 code to date of birth of subject.
 
     Parameters
     ----------
-    excel_path : str
-        Path to Excel file with authoritative dates of birth.
-    ace_iq_path : str
-        Path to ACE-IQ CSV file exported from Psytools.
-    phir_path : str
-        Path to PHIR CSV file exported from Psytools.
+    recruitment_data : pandas.DataFrame
+        Data read from recruitment files.
 
     Returns
     -------
@@ -154,114 +114,40 @@ def _initialize_dob_from_psc1(excel_paths, ace_iq_path, phir_path):
         Maps PSC1 code to date of birth of subject.
 
     """
+    recruitment_data = _read_recruitment_files(paths)
+
     dob_from_psc1 = {}
-
-    excel = {}
-    for excel_path in excel_paths:
-        excel.update(_read_excel(excel_path))
-    ace_iq_questions = {'ACEIQ_C2': 'datetime.date'}
-    ace_iq = read_psytools(ace_iq_path, ace_iq_questions)
-    phir_questions = {'PHIR_01': 'datetime.date'}
-    phir = read_psytools(phir_path, phir_questions)
-    for psc1 in set(excel) | set(ace_iq) | set(phir):
-        if psc1 in excel:
-            dob_excel = excel[psc1]
-        else:
-            dob_excel = None
-        if psc1 in ace_iq and 'ACEIQ_C2' in ace_iq[psc1]:
-            dob_ace_iq = ace_iq[psc1]['ACEIQ_C2']
-        else:
-            dob_ace_iq = None
-        if psc1 in phir and 'PHIR_01' in phir[psc1]:
-            dob_phir = phir[psc1]['PHIR_01']
-        else:
-            dob_phir = None
-        if dob_excel:
-            dob_from_psc1[psc1] = dob_excel
-            if dob_ace_iq and dob_ace_iq != dob_excel:
-                logger.info('ACE-IQ date of birth %s different from reference %s: %s',
-                            dob_ace_iq, dob_excel, psc1)
-            if dob_phir and dob_phir != dob_excel:
-                logger.info('PHIR date of birth %s different from reference %s: %s',
-                            dob_phir, dob_excel, psc1)
-        elif dob_ace_iq and dob_phir:
-            if dob_ace_iq == dob_phir:
-                dob_from_psc1[psc1] = dob_ace_iq
-            else:
-                logger.error('inconsistent dates in ACE-IQ (%s) and PHIR (%s): %s',
-                             dob_ace_iq, dob_phir, psc1)
-        elif dob_ace_iq:
-            logger.warn('missing PHIR and reference date of birth for: %s', psc1)
-            dob_from_psc1[psc1] = dob_ace_iq
-        elif dob_phir:
-            logger.warn('missing ACE-IQ and reference date of birth for: %s', psc1)
-            dob_from_psc1[psc1] = dob_phir
-        else:  # huh? shouldn't be here if PSC1 found in a source at least
-            logger.error('missing any date of birth for: %s', psc1)
-
-    return dob_from_psc1
-
-
-def _initialize_sex_from_psc1(ace_iq_path, pds_path, sdim_path):
-    """Build dictionnary to map PSC1 code to sex of subject.
-
-    Parameters
-    ----------
-    ace_iq_path : str
-        Path to ACE-IQ CSV file exported from Psytools.
-    pds_path : str
-        Path to PDS CSV file exported from Psytools.
-    sdim_path : str
-        Path to SDIM CSV file exported from Psytools.
-
-    Returns
-    -------
-    dict
-        Maps PSC1 code to sex of subject.
-
-    """
     sex_from_psc1 = {}
+    for row in recruitment_data.itertuples(index=False):
+        psc1 = row.PSC1
 
-    ace_iq_questions = {'ACEIQ_C1': None}
-    ace_iq = read_psytools(ace_iq_path, ace_iq_questions)
-    pds_questions = {'PDS_gender': None}
-    pds = read_psytools(pds_path, pds_questions)
-    sdim_questions = {'SDI_02': None}
-    sdim = read_psytools(sdim_path, sdim_questions)
-    for psc1 in list(ace_iq) + list(pds) + list(sdim):
-        merge = {}
-        if psc1 in ace_iq and 'ACEIQ_C1' in ace_iq[psc1]:
-            ace_iq_sex = ace_iq[psc1]['ACEIQ_C1']
-            merge[ace_iq_sex] = merge.setdefault(ace_iq_sex, 0) + 1
-        if psc1 in pds and 'PDS_gender' in pds[psc1]:
-            pds_sex = pds[psc1]['PDS_gender']
-            merge[pds_sex] = merge.setdefault(pds_sex, 0) + 1
-        if psc1 in sdim and 'SDI_02' in sdim[psc1]:
-            sdim_sex = sdim[psc1]['SDI_02']
-            merge[sdim_sex] = merge.setdefault(sdim_sex, 0) + 1
-        if len(merge) < 1:
-            logger.warning('missing sex for: %s', psc1)
-        elif len(merge) > 1:
-            logger.info('inconsistent sex for: %s', psc1)
-            # find the value that occurred more than any other
-            reverse = {}
-            for k, v in merge.items():
-                reverse.setdefault(v, []).append(k)
-            m = max(reverse.keys())
-            if len(reverse[m]) == 1:
-                sex_from_psc1[psc1] = reverse[m][0]
+        if pandas.isnull(row.DOB):
+            logger.error('%s: invalid value for date of birth: %s', psc1, row.DOB)
         else:
-            sex_from_psc1[psc1] = list(merge.keys())[0]
+            dob = row.DOB.date()
+            if psc1 not in dob_from_psc1:
+                dob_from_psc1[psc1] = dob
+            elif dob == dob_from_psc1[psc1]:
+                logger.warning('%s: duplicate PSC1 code in recruitment file', psc1)
+            else:
+                logger.error('%s: inconsistent duplicate PSC1 code in recruitment file', psc1)
 
-    return sex_from_psc1
+        if row.SEX not in {'F', 'M'}:
+            logger.error('%s: invalid value for sex: "%s"', psc1, row.SEX)
+        elif psc1 not in sex_from_psc1:
+            sex_from_psc1[psc1] = row.SEX
+        elif row.SEX == sex_from_psc1[psc1]:
+            logger.warning('%s: duplicate PSC1 code in recruitment file', psc1)
+        else:
+            logger.error('%s: inconsistent duplicate PSC1 code in recruitment file', psc1)
+
+    return dob_from_psc1, sex_from_psc1
 
 
 PSC2_FROM_PSC1 = _initialize_psc2_from_psc1(_PSC_PATH)
 PSC1_FROM_PSC2 = {v: k for k, v in PSC2_FROM_PSC1.items()}
-_recruitment_files = (os.path.join(_RECRUITMENT_FILES_DIR, recruitment_file)
-                      for recruitment_file in _RECRUITMENT_FILES)
-DOB_FROM_PSC1 = _initialize_dob_from_psc1(_recruitment_files, _ACE_IQ_PATH, _PHIR_PATH)
-SEX_FROM_PSC1 = _initialize_sex_from_psc1(_ACE_IQ_PATH, _PDS_PATH, _SDIM_PATH)
+DOB_FROM_PSC1, SEX_FROM_PSC1 = _initialize_dob_sex(os.path.join(_RECRUITMENT_FILES_DIR, f)
+                                                   for f in _RECRUITMENT_FILES)
 
 
 def age_band(age):
---
+++ freeze/cveda_freeze_psytools.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2018 CEA
+#
+# This software is governed by the CeCILL license under French law and
+# abiding by the rules of distribution of free software. You can use,
+# modify and/ or redistribute the software under the terms of the CeCILL
+# license as circulated by CEA, CNRS and INRIA at the following URL
+# "http://www.cecill.info".
+#
+# As a counterpart to the access to the source code and rights to copy,
+# modify and redistribute granted by the license, users are provided only
+# with a limited warranty and the software's author, the holder of the
+# economic rights, and the successive licensors have only limited
+# liability.
+#
+# In this respect, the user's attention is drawn to the risks associated
+# with loading, using, modifying and/or developing or reproducing the
+# software by the user in light of its specific status of free software,
+# that may mean that it is complicated to manipulate, and that also
+# therefore means that it is reserved for developers and experienced
+# professionals having in-depth computer knowledge. Users are therefore
+# encouraged to load and test the software's suitability as regards their
+# requirements in conditions enabling the security of their systems and/or
+# data to be ensured and, more generally, to use and operate it in the
+# same conditions as regards security.
+#
+# The fact that you are presently reading this means that you have had
+# knowledge of the CeCILL license and that you accept its terms.
+
+
+import pandas
+import os
+from cveda_databank import PSC2_FROM_PSC1
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+# frozen PSC1 codes
+_EXCEL_FREEZE_FILE = '/cveda/databank/framework/meta_data/freeze/1.0/cVEDA_PSC_Data_Freeze.xlsx'
+
+# in/out paths
+_IN_PATH = '/cveda/databank/processed/psytools'
+_OUT_PATH = '/cveda/databank/1.0/psytools'
+
+
+def read_freeze(path):
+    with pandas.ExcelFile(path) as excel_file:
+        converters = {
+            'PSC1': str,
+        }
+        df = pandas.read_excel(path, converters=converters)
+        return df['PSC1'].tolist()
+
+
+def read_psc1_codes(files):
+    result = []
+    for path in files:
+        with open(path) as f:
+            for line in f:
+                result.append(line.strip())
+    return result
+
+
+def main():
+    # frozen participants
+    frozen = set(PSC2_FROM_PSC1[psc1] for psc1
+                 in read_freeze(_EXCEL_FREEZE_FILE))
+
+    # keep only frozen participants
+    for psytools in os.listdir(_IN_PATH):
+        inpath = os.path.join(_IN_PATH, psytools)
+        outpath = os.path.join(_OUT_PATH, psytools)
+        with open(inpath, 'r') as infile, open(outpath, 'w') as outfile:
+            line = next(infile)
+            outfile.write(line)
+            for line in infile:
+                psc2 = line[:12]
+                if psc2 in frozen:
+                    outfile.write(line)
+
+
+if __name__ == "__main__":
+    main()
